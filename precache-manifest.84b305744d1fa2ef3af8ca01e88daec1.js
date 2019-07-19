fac87e5d97ae088f5e4d3c92685a0088168fd6e3
YousraKhan <yousra.khan97@hotmail.com>
2019-07-20 02:40:59 +0500

    Updates

---
+++ precache-manifest.84b305744d1fa2ef3af8ca01e88daec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706c08c673fb4a94df2103d34e2f8a0a",
+    "url": "/facerecoapp/index.html"
+  },
+  {
+    "revision": "d09e70ac6e3d34137d82",
+    "url": "/facerecoapp/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3096dc9e365e308ed535",
+    "url": "/facerecoapp/static/css/main.cff5eb8f.chunk.css"
+  },
+  {
+    "revision": "d09e70ac6e3d34137d82",
+    "url": "/facerecoapp/static/js/2.650421db.chunk.js"
+  },
+  {
+    "revision": "3096dc9e365e308ed535",
+    "url": "/facerecoapp/static/js/main.c96ad1c5.chunk.js"
+  },
+  {
+    "revision": "d73af0868c06bfedb0db",
+    "url": "/facerecoapp/static/js/runtime~main.deb2084e.js"
+  }
+]);
